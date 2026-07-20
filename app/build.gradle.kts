917d306831ba02021ce619d5da8c0d6c5590d086
valera2872 <barbarov28@protonmail.com>
2026-07-20 16:42:22 +0300

    Bump Combat Performance prototype to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.valera2872.grapplingarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.3.3"
+        versionCode = 7
+        versionName = "0.4.0"
     }
 
     buildTypes {
