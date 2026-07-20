523fed1a60f1f4d3642ed28dd190332650412410
valera2872 <barbarov28@protonmail.com>
2026-07-20 12:35:08 +0300

    Confirm fixed signing in version 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.valera2872.grapplingarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
     }
 
     buildTypes {
