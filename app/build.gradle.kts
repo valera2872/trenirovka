89291c93226e74d30678736f07ed80705adf1916
valera2872 <barbarov28@protonmail.com>
2026-07-28 16:36:37 +0300

    Bump hotfix release to 0.9.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.valera2872.grapplingarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.9.3"
+        versionCode = 24
+        versionName = "0.9.4"
     }
 
     buildTypes {
