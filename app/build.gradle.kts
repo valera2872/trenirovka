36ecadbc76df0114d18312fc7ac34381fc8efa08
valera2872 <barbarov28@protonmail.com>
2026-07-28 23:57:52 +0300

    Bump Combat Performance to 0.9.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.valera2872.grapplingarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.9.5"
+        versionCode = 26
+        versionName = "0.9.6"
     }
 
     buildTypes {
