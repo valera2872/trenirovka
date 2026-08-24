9b2448cca6674208612dd81657a52d5b4b7ce5f3
valera2872 <barbarov28@protonmail.com>
2026-08-24 09:48:20 +0300

    Bump premium UI build to 0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.valera2872.grapplingarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "0.9.7"
+        versionCode = 28
+        versionName = "0.10.0"
     }
 
     buildTypes {
