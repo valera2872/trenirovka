f2833ba619cbd4333315071f6e5344ccdd4d1b5b
valera2872 <barbarov28@protonmail.com>
2026-08-24 14:40:03 +0300

    Bump version to 0.10.1 for legacy launcher fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.valera2872.grapplingarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "0.10.0"
+        versionCode = 29
+        versionName = "0.10.1"
     }
 
     buildTypes {
